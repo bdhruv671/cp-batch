566e010e591b5c4ccce7978fa94dff0d50224492
Dhruv Bisht <bdhruv671@gmail.com>
2025-01-21 14:30:21 +0530

    code

--- vec.cpp
+++ vec.cpp
@@ -1,3 +1,14 @@
+// Write a C++ program that demonstrates the concept of dynamic arrays using std::vector. The program should:
+
+// Prompt the user to enter the number of elements they want to store in a dynamic array (vector).
+// Use a std::vector to store the elements entered by the user.
+// Allow the user to input the elements of the vector.
+// Display the elements of the vector.
+// Include functionality to add a new element to the vector and display the updated vector."
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std ;
 int main (){
